5f2c8181597b2c590ee9c9a4a0fa2d1bda93bd91
Kunal Varkekar <pvarkeka@uwaterloo.ca>
2025-01-13 09:28:05 -0500

    Added blog for chatique

--- chatique/blog/main.js
+++ chatique/blog/main.js
@@ -1,9 +1,14 @@
 import './main.css'
+import '../header/header.css'
 
 
 import mainHTML from "./main.html";
+import headerHTML from '../header/header.html'
 
 // Insert the main pages into app
 const app = document.querySelector("#app")
 app.innerHTML = mainHTML
 
+// Insert individual elements 
+const header = app.querySelector("#header")
+header.innerHTML = headerHTML
